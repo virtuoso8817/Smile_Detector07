53428e73b2cf899c2f83d72a076a577d2dfc99d9
Om Aditya <132847025+virtuoso8817@users.noreply.github.com>
2025-07-09 03:53:58 +0530

    Final Update

--- README.md
+++
@@ -1,2 +0,0 @@
-# Smile_Detector07
-Open_cv based project based on haar_cascade classifier for smile detection on video and still frames
--- main.py
+++ main.py
@@ -1,1 +1,45 @@
+import cv2
+import numpy as np
 
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_smile.xml')
+
+def detect(gray, frame):
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), ((x + w), (y + h)), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+        smiles = smile_cascade.detectMultiScale(roi_gray, 1.8, 30)
+        fps = video_capture.get(cv2.CAP_PROP_FPS)
+        cv2.putText(frame, f'FPS: {fps}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+        for (sx, sy, sw, sh) in smiles:
+            cv2.rectangle(roi_color, (sx, sy), ((sx + sw), (sy + sh)), (0, 0, 255), 2)
+            cv2.putText(roi_color, 'Smile', (sx, sy - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+
+    return frame
+
+
+video_capture = cv2.VideoCapture(0)
+while video_capture.isOpened():
+    # Captures video_capture frame by frame
+    _, frame = video_capture.read()
+
+    # To capture image in monochrome
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # calls the detect() function
+    canvas = detect(gray, frame)
+
+    # Displays the result on camera feed
+    cv2.imshow('Video', canvas)
+
+    # The control breaks once q key is pressed
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+
+# Release the capture once all the processing is done.
+video_capture.release()
+cv2.destroyAllWindows()
